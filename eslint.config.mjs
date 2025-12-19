41a64dd4d73a07480dc11d24fa78a763761fbfe9
sthDINESH <sthapit.dnsh@gmail.com>
2025-12-19 17:36:21 +0000

    add lint setup

---
+++ eslint.config.mjs
@@ -0,0 +1,34 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import { defineConfig } from 'eslint/config'
+import stylisticJs from '@stylistic/eslint-plugin'
+
+export default defineConfig([
+  {
+    ignores: ['dist/**'],
+  },
+  {
+    files: ['**/*.{js,mjs,cjs}'],
+    plugins: {
+      js,
+      '@stylistic/js': stylisticJs,
+    },
+    extends: ['js/recommended'],
+    languageOptions: {
+      globals: globals.node,
+      ecmaVersion: 'latest'
+    },
+    rules: {
+      '@stylistic/js/indent': ['error', 2],
+      '@stylistic/js/linebreak-style': ['error', 'unix'],
+      '@stylistic/js/quotes': ['error', 'single'],
+      '@stylistic/js/semi': ['error', 'never'],
+      eqeqeq: 'error',
+      'no-trailing-spaces': 'error',
+      'object-curly-spacing': ['error', 'always'],
+      'arrow-spacing': ['error', { before: true, after: true }],
+      'no-console': 'off',
+    },
+  },
+  { files: ['**/*.js'], languageOptions: { sourceType: 'commonjs', ecmaVersion: 'latest' } },
+])
--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send('<h1>Hello from server</h1>')
+  res.send('<h1>Hello from server</h1>')
 })
 
 const PORT = 3001
-app.listen(PORT, ()=>{
-    console.log(`Starting Express server on ${PORT}...`)
+app.listen(PORT, () => {
+  console.log(`Starting Express server on ${PORT}...`)
 })
