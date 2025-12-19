e45dbbca931e4d958e796c41f28d3c82f573ca0d
sthDINESH <sthapit.dnsh@gmail.com>
2025-12-19 22:46:08 +0000

    add morgan setup for request logging

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const express = require('express')
 const app = express()
+const morgan = require('morgan')
 
-// Middlewares
+
+
+/**
+ * Middlewares
+ * */
 app.use(express.json())
+
+// Morgan
+morgan.token('body', (req, res) => JSON.stringify(req.body))
+app.use(morgan(':date :method :url :status :res[content-length] - :response-time ms :body'))
 
 app.get('/', (req, res) => {
   res.send('<h1>Hello from server</h1>')
