eb475ae140b8646317801e6eb4dd7dc25f171417
sthDINESH <sthapit.dnsh@gmail.com>
2025-12-19 17:03:55 +0000

    inital express server setup checkin

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('<h1>Hello from server</h1>')
+})
+
+const PORT = 3001
+app.listen(PORT, ()=>{
+    console.log(`Starting Express server on ${PORT}...`)
+})
