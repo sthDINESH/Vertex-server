feddad882d014306099b9e1085ca798c83763431
sthDINESH <sthapit.dnsh@gmail.com>
2025-12-23 22:26:50 +0000

    fix lint error

--- index.js
+++ index.js
@@ -2,15 +2,13 @@ const express = require('express')
 const app = express()
 const morgan = require('morgan')
 
-
-
 /**
  * Middlewares
  * */
 app.use(express.json())
 
 // Morgan
-morgan.token('body', (req, res) => JSON.stringify(req.body))
+morgan.token('body', (req) => JSON.stringify(req.body))
 app.use(morgan(':date :method :url :status :res[content-length] - :response-time ms :body'))
 
 app.get('/', (req, res) => {
