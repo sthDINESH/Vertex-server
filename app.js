0edff10f97c1f591736ca5bc0211937e6f98d7c2
sthDINESH <sthapit.dnsh@gmail.com>
2025-12-25 15:30:45 +0000

    refactor to add project structure

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const cors = require('cors')
+const morgan = require('morgan')
+const healthRouter = require('./controllers/health')
+
+const app = express()
+
+/**
+ * Middlewares
+ * */
+const corsOptions = {
+  origin:[
+    /localhost/,
+  ]
+}
+app.use(cors(corsOptions))
+app.use(express.json())
+// Morgan
+morgan.token('body', (req) => JSON.stringify(req.body))
+app.use(morgan(':date :method :url :status :res[content-length] - :response-time ms :body'))
+
+/**
+ * Routes
+ */
+app.use('/api/health', healthRouter)
+
+module.exports = app
--- index.js
+++ index.js
@@ -1,34 +1,9 @@
 const config = require('./utils/config')
-const express = require('express')
-const cors = require('cors')
-const morgan = require('morgan')
-const healthRouter = require('./controllers/health')
+const logger = require('./utils/logger')
 
-const app = express()
-
-/**
- * Middlewares
- * */
-const corsOptions = {
-  origin:[
-    /localhost/,
-  ]
-}
-app.use(cors(corsOptions))
-app.use(express.json())
-
-// Routes
-app.use('/api/health', healthRouter)
-
-// Morgan
-morgan.token('body', (req) => JSON.stringify(req.body))
-app.use(morgan(':date :method :url :status :res[content-length] - :response-time ms :body'))
-
-app.get('/', (req, res) => {
-  res.send('<h1>Hello from server</h1>')
-})
+const app = require('./app')
 
 const PORT = config.PORT
 app.listen(PORT, () => {
-  console.log(`Starting Express server on ${PORT}...`)
+  logger.info(`Starting Express server on ${PORT}...`)
 })
